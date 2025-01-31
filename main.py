c5b9197ecbb459bc4bab3ecb9fe1c98fd00fd8ae
kameil <racovikpvpbr@gmail.com>
2025-01-31 00:24:41 -0300

    get preview

--- main.py
+++ main.py
@@ -10,19 +10,7 @@ app = Flask(__name__)
 walpapers_dir = "C:/Program Files (x86)/Steam/steamapps/workshop/content/431960/"
 
 
-def zip_folder_in_memory(folder_path):
-    # Cria um arquivo ZIP em memória
-    zip_buffer = io.BytesIO()
 
-    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                # Adiciona ao arquivo ZIP, preservando a estrutura de diretórios
-                zipf.write(file_path, os.path.relpath(file_path, folder_path))
-
-    zip_buffer.seek(0)  # Volta para o início do buffer para envio
-    return zip_buffer
 
 
 @app.route('/')
@@ -39,19 +27,28 @@ def style():
         return file.read()
 
 
+
+
+
 from get_walpappers import get_walpappers
 
 @app.route("/get_walpapers")
 def getwalpp():
     return jsonify(get_walpappers())
 
+from get_pastezip import zip_folder_in_memory
+
 @app.route("/download_walpaper/<walpaper_id>")
 def dowwappp(walpaper_id):
     folder_path = f"{walpapers_dir}{walpaper_id}"
     zip_buffer = zip_folder_in_memory(folder_path)
     return send_file(zip_buffer, as_attachment=True, download_name='arquivo.zip', mimetype='application/zip')
-    
-    
+#getpreview    
+
+@app.route("/preview/<walpaper_id>/<preview_file>")
+def getpreview(walpaper_id, preview_file):
+    return send_file(f"{walpapers_dir}{walpaper_id}/{preview_file}")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
